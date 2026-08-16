22d7e6a8f25af5f6a8197e20e61abb038f393c09
Axel Araujo <axelaraujor@gmail.com>
2026-08-16 15:50:34 -0500

    1era sesion de aprendizaje con Python

--- python-files/prueba1.py
+++ python-files/prueba1.py
@@ -2,5 +2,7 @@
 name = input("ingresa tu nombre: ")
 print("hola " + name)
 ganas = input("cuantas ganas tienes de aprender?: ")
-print("tienes " + ganas + " ganas de aprender?, excelente!")
+print("tienes " + ganas * 3 + " ganas de aprender?, excelente!")
 print("no drama")
+print("presiona enter para terminar esta funcion")
+input()
--- python-files/prueba2.py
+++ python-files/prueba2.py
@@ -1,1 +1,10 @@
 # hola, listo para lo que sigue?
+numeros2 = []
+for i in range(5):
+    numeros2.append(i)
+print(numeros2)    
+del numeros2[0:3]
+print(numeros2)
+del numeros2
+print(numeros2)
+# dejemoslo aqui, finalizo mi sesion a las 3:50 pm
